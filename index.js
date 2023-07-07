7c01581cdf62275cdedf7df8c172ac909c327ffe
bukakesh <46136045+bukakesh@users.noreply.github.com>
2023-07-07 15:06:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser.json());
-
-app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
-});
-
-app.listen(3000, () => console.log('Server started on port 3000'));
